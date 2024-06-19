43925d1add87b5469dc569bd889737c5bd86cb14
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-06-19 08:29:11 +0000

    Build entire components/sections structure for the project

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import PopularProducts from './PopularProducts';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import Nav from './Nav';
+
+export {
+    Hero,
+    CustomerReviews,
+    Footer,
+    Services,
+    SpecialOffer,
+    PopularProducts,
+    Subscribe,
+    SuperQuality,
+    Nav
+}
