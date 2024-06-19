855f73158aac4184982e5bd7dfb0b483368777f0
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-06-19 15:27:02 +0000

    Retouching the hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { arrowRight } from "../assets/icons";
 import { bigShoe1 } from "../assets/images";
-import Button from "../components/Button";
-import ShoeCard from "../components/ShoeCard";
+import { Button, ShoeCard } from "../components";
 import { shoes, statistics } from "../constants";
 
 const Hero = () => {
@@ -23,9 +22,9 @@ const Hero = () => {
                     <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
                 </h1>
                 <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life</p>
-                <Button label="Show now" iconURL={arrowRight}/>
+                <Button label="Shop now" iconURL={arrowRight} />
                 
-                <div className="flex justify-start flex-wrap w-full mt-20 gap-16">
+                <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
                     {statistics.map((stats) => (
                         <div key={stats.label}>
                             <p className="text-4xl font-palanquin font-bold">{stats.value}</p>
@@ -34,19 +33,22 @@ const Hero = () => {
                     ))}
                 </div>
             </div>
+
             <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
                 <img 
                     src={bigShoeImg} 
                     alt="shoe collection"
                     width={610}
-                    height={500}
+                    height={502}
                     className="object-contain relative z-10" 
                 />
+
                 <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
-                    {shoes.map((shoe) => (
-                        <div key={shoe}>
+                    {shoes.map((image, index) => (
+                        <div key={index}>
                             <ShoeCard
-                                imgURL={shoe}
+                                index={index}
+                                imgURL={image}
                                 changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
                                 bigShoeImage={bigShoeImg}
                             />
