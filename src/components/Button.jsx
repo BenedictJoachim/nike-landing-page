9e0dc1dcaeddd89ebe4e9c9f83b707e55f7eb721
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-06-19 20:31:05 +0000

    Build the SpecialOffer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,12 @@
-const Button = ({ label, iconUrl }) => {
+const Button = ({ label, iconUrl, backgroundColor, borderColor, textColor, fullwidth }) => {
     return (
-        <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red ">
+        <button className={`
+            flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+            ${
+                backgroundColor
+                 ? `${backgroundColor} ${textColor} ${borderColor}`
+                 : "bg-coral-red  text-white border-coral-red"
+                 } rounded-full ${fullwidth && "w-full"}`}>
             {label}
             {iconUrl && <img 
                 src={iconUrl}
