f03abd4421a6c3bdb9f9cd28e07121a89e2295a4
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-06-19 19:16:58 +0000

    Build the SuperQuality section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { arrowRight } from "../assets/icons";
 import { bigShoe1 } from "../assets/images";
 import { Button, ShoeCard } from "../components";
 import { shoes, statistics } from "../constants";
+import { arrowRight } from "../assets/icons";
 
 const Hero = () => {
     const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
